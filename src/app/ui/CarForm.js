2e9431bbf61049c4fb261956f6d5f2889348d1f0
vin-vu <vince.hvu@gmail.com>
2024-05-23 16:44:33 -0700

    Refactored CarForm and added some styling

--- src/app/ui/CarForm.js
+++ src/app/ui/CarForm.js
@@ -1,45 +1,36 @@
 'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
+import styles from './CarForm.module.css';
 
 export default function CarForm() {
+  const [formData, setFormData] = useState({
+    make: '',
+    model: '',
+    year: '',
+    'License Plate': '',
+    vin: '',
+  });
 
-    const [formData, setFormData] = useState({
-        'make': '',
-        'model': '',
-        'year': '',
-        'license plate': '',
-        'vin': ''
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((previousFormData) => ({ ...previousFormData, [name]: value }));
+  };
 
-    });
-
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((previousFormData) => ({...previousFormData, [name]: value}))
-    };
-        
-    return (
-        <form>
-            <label>
-                Make:{' '}
-                <input type="text" name="make" value={formData.make} onChange={handleChange} />
-            </label>
-            <label>
-                Model:{' '}
-                <input type="text" name="model" value={formData.model} onChange={handleChange} />
-            </label>
-            <label>
-                Year:{' '}
-                <input type="text" name="year" value={formData.year} onChange={handleChange} />
-            </label>
-            <label>
-                License Plate:{' '}
-                <input type="text" name="license plate" value={formData["license plate"]} onChange={handleChange} />
-            </label>
-            <label>
-                VIN:{' '}
-                <input type="text" name="vin" value={formData.vin} onChange={handleChange} />
-            </label>
-        </form>
-    );
+  return (
+      <form className={styles.FormContainer}>
+        {Object.keys(formData).map((key) => (
+          <div className={styles.FormElement} key={key}>
+            <label>{key[0].toLocaleUpperCase() + key.slice(1)}</label>
+            <input
+              type="text"
+              name={key}
+              value={formData[key]}
+              onChange={handleChange}
+              placeholder={' Enter ' + key}
+            />
+          </div>
+        ))}
+      </form>
+  );
 }
