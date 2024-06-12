1b00c1df38c5836ed342b4fa34aa3a02a831d8e9
vin-vu <vince.hvu@gmail.com>
2024-06-11 22:24:26 -0700

    Added dark mode as default theme

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,7 @@
-import CarForm from './ui/CarForm';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import CarActionsContainer from './ui/CarActionsContainer';
 
 export default function Home() {
-  return (
-    <>
-      <CarActionsContainer />
-      <div className="flex justify-center items-center">
-        {/* <TriggerButton name={"Add Car"} /> */}
-      </div>
-      <CarForm />
-    </>
-  );
+  return <CarActionsContainer />;
 }
